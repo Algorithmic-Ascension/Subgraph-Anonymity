b48176f0e22cda85d34e6d744f5f9eece1a58691
Kenshin Maybe <kenshin@builds-haku>
2014-04-23 02:06:40 -0400

    progress was made

--- CS565.py
+++ CS565.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 n = 120
 s = 3
 g = nx.powerlaw_cluster_graph(n, 5, 0.5)
-r = g.node.keys()
+r = g.nodes()
 h = nx.Graph()
 supernodes = []
 
 #node correspondence
 for i in range(n/s):
 	h.add_node(i) # supernode
-	supernodes[i] = [r[0], r[0].neighobors[0], r[0].neighbors[1]]
+	remaining = [x for x in set(g.neighbors(r[0])) & set(r)]
+	supernodes[i] = nx.Graph(g.subgraph( [r[0], remaining[0], remaining[1]] ))
+	#supernodes[i] = nx.Graph(g.subgraph( [r[0]] + remaining[0:s-1] ))
 
-	#TODO extend to for loop from s-1 to 0 
+	#TODO test later, note need to remove in order
+	#for (j=s-1; j>0; j--):
+	#	r.remove(r[0].neighbors[j])
 	r.remove(r[0].neighobors[1])
 	r.remove(r[0].neighobors[0])
 	r.remove(r[0])
 
 for i in range(n/s):
-	h.add_weighted_edges_from([()])
+	h.add_weighted_edges_from([(i,j, ) for j in ])
